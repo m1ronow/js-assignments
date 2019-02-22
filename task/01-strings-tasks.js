beccae1bcd07a852dfd8c97d44a9e8def10f768c
Ivan Mironov <45268080+m1ronow@users.noreply.github.com>
2019-02-23 01:49:42 +0300

    encodetoRot13 example

--- task/01-strings-tasks.js
+++ task/01-strings-tasks.js
@@ -220,7 +220,7 @@ function getRectangleString(width, height) {
  *
  */
 function encodeToRot13(str) {
-  throw new Error('Not implemented');
+  return str.replace(/[a-zA-Z]/g,function(c){return String.fromCharCode((c <= "Z"?90:122) >= (c = c.charCodeAt(0) + 13)?c:c - 26);});
 }
 
 /**
